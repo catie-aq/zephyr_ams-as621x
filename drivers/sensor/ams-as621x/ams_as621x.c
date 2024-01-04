3577320ec34e28f1c0f9b308a8e8f64f89d027fd
Clément Laigle <c.laigle@catie.fr>
2024-01-04 15:22:55 +0100

    Add temperature reading

--- drivers/sensor/ams-as621x/ams_as621x.c
+++ drivers/sensor/ams-as621x/ams_as621x.c
@@ -11,17 +11,63 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(ams_as621x, CONFIG_SENSOR_LOG_LEVEL);
 
-struct as621x_dev_cfg {
-	struct i2c_dt_spec i2c_port;
+#define TVAL   0x0
+#define CONFIG 0x1
+#define TLOW   0x2
+#define THIGH  0x3
+
+struct as621x_cfg {
+	struct i2c_dt_spec i2c;
+};
+
+struct as621x_data {
+	int16_t temp;
 };
 
 static int as621x_fetch(const struct device *dev, enum sensor_channel chan)
 {
-	return 0;
+	struct as621x_data *dev_data = dev->data;
+	const struct as621x_cfg *dev_cfg = dev->config;
+
+	int16_t value;
+	int ret;
+
+	if (chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_AMBIENT_TEMP) {
+		return -ENOTSUP;
+	}
+
+	ret = i2c_write_read_dt(&dev_cfg->i2c, TVAL, 1, (uint8_t *)&value, sizeof(value));
+	if (!ret) {
+		dev_data->temp = value;
+	}
+
+	return ret;
 }
 
 static int as621x_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
 {
+	struct as621x_data *dev_data = dev->data;
+
+	if (chan != SENSOR_CHAN_AMBIENT_TEMP) {
+		return -ENOTSUP;
+	}
+
+	double value = (double)dev_data->temp / 128.0;
+	val->val1 = (int)value;
+	val->val2 = (value - val->val1) * 1000000;
+
+	return 0;
+}
+
+static int as621x_init(const struct device *dev)
+{
+	const struct as621x_cfg *cfg = dev->config;
+
+	if (!i2c_is_ready_dt(&cfg->i2c)) {
+		LOG_ERR("I2C bus %s not ready", cfg->i2c.bus->name);
+		return -ENODEV;
+	}
+
 	return 0;
 }
 
@@ -31,8 +77,8 @@ static const struct sensor_driver_api as5600_driver_api = {
 };
 
 #define AS621X_INIT(n)                                                                             \
-	static struct as621x_dev_cfg as621x_config_##n = {                                         \
-		.i2c_port = I2C_DT_SPEC_INST_GET(n),                                               \
+	static struct as621x_cfg as621x_config_##n = {                                             \
+		.i2c = I2C_DT_SPEC_INST_GET(n),                                                    \
 	};                                                                                         \
 	static struct as621x_data as621x_data_##n;                                                 \
 	DEVICE_DT_INST_DEFINE(n, as621x_init, NULL, &as621x_data_##n, &as621x_config_##n,          \
