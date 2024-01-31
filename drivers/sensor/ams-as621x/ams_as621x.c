7b4236d22d77d47b6cdd57db244c08dfd110a892
Clément Laigle <c.laigle@catie.fr>
2024-01-31 11:02:29 +0100

    Add sample rate configuration options and add header file

--- drivers/sensor/ams-as621x/ams_as621x.c
+++ drivers/sensor/ams-as621x/ams_as621x.c
@@ -10,12 +10,9 @@
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(AS621X, CONFIG_SENSOR_LOG_LEVEL);
+#include "ams_as621x.h"
 
-#define TVAL   0x0
-#define CONFIG 0x1
-#define TLOW   0x2
-#define THIGH  0x3
+LOG_MODULE_REGISTER(AS621X, CONFIG_SENSOR_LOG_LEVEL);
 
 struct as621x_cfg {
 	struct i2c_dt_spec i2c;
@@ -25,7 +22,24 @@ struct as621x_data {
 	uint16_t temp;
 };
 
-static int as621x_fetch(const struct device *dev, enum sensor_channel chan)
+static int as621x_set_conv_rate(const struct device *dev, uint8_t rate)
+{
+	const struct as621x_cfg *cfg = dev->config;
+	uint16_t config;
+
+	int ret = i2c_burst_read_dt(&cfg->i2c, AS621X_REG_CONFIG, (uint8_t *)&config, 2);
+
+	if (ret) {
+		return -EIO;
+	}
+
+	config &= ~(0x3 << 6);
+	config |= (rate << 6);
+
+	return i2c_burst_write_dt(&cfg->i2c, AS621X_REG_CONFIG, (uint8_t *)&config, 2);
+}
+
+static int as621x_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
 	struct as621x_data *dev_data = dev->data;
 	const struct as621x_cfg *dev_cfg = dev->config;
@@ -37,7 +51,7 @@ static int as621x_fetch(const struct device *dev, enum sensor_channel chan)
 		return -ENOTSUP;
 	}
 
-	ret = i2c_burst_read_dt(&dev_cfg->i2c, TVAL, (uint8_t *)&value, 2);
+	ret = i2c_burst_read_dt(&dev_cfg->i2c, AS621X_REG_TVAL, (uint8_t *)&value, 2);
 	if (!ret) {
 		value = (value >> 8) | (value << 8);
 		dev_data->temp = value;
@@ -46,7 +60,8 @@ static int as621x_fetch(const struct device *dev, enum sensor_channel chan)
 	return ret;
 }
 
-static int as621x_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+static int as621x_channel_get(const struct device *dev, enum sensor_channel chan,
+			      struct sensor_value *val)
 {
 	struct as621x_data *dev_data = dev->data;
 
@@ -70,12 +85,17 @@ static int as621x_init(const struct device *dev)
 		return -ENODEV;
 	}
 
+	if (as621x_set_conv_rate(dev, AS621X_CONVERSION_RATE)) {
+		LOG_ERR("Failed to set conversion rate");
+		return -EIO;
+	}
+
 	return 0;
 }
 
 static const struct sensor_driver_api as621x_driver_api = {
-	.sample_fetch = as621x_fetch,
-	.channel_get = as621x_get,
+	.sample_fetch = as621x_sample_fetch,
+	.channel_get = as621x_channel_get,
 };
 
 #define AS621X_INIT(n)                                                                             \
